778e44370635c5cde1d182ad69d716f4279c0bee
unknown <haris.anwar1986@gmail.com>
2023-10-07 18:16:51 +0200

    bootup code

--- src/kernel.c
+++ src/kernel.c
@@ -5,10 +5,10 @@ void kernel_main(){
     uart_init();
     uart_send_string("From Raspberry PI Bare Metal\n");
 
-    #if RASP_VERSIOn == 3
+    #if RPI_VERSIOn == 3
     uart_send_string("\tFROM Board 3\n");
     #endif
-    #if RASP_VERSIOn == 4
+    #if RPI_VERSIOn == 4
     uart_send_string("\tFROM Board 4\n");
     #endif
 
